d15375326c055349b0ac39566d7d96acc3c8eb94
Kevin Buffardi <kbuffardi@csuchico.edu>
2017-10-03 15:47:40 -0700

    Main.cpp with description of what needs to be done

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  vector <vector <string> > wordGrid;
+
+  /*
+     do not edit this space
+
+     where this comment is, the instructor will
+     add the code that gets the user's input and
+     inserts the input into every element of the
+     matrix wordGrid
+  */
+
+  //PUT YOUR CODE BELOW THIS LINE
+  //that prints each element of the matrix in the
+  //following format:
+  //Row _ Column _ is _
+  //the first and second blanks should be replaced with the coordinates
+  //of the element's row and column and the third blank should be replaced
+  //with the value of that element
+
+  
+
+
+
+ return 0;
+}
+
+
+
+
